896bfc80030267804d6ee35eef8186f7e086df15
meghost77 <vadim.mirvald@freelancehunt.com>
2024-06-20 11:13:47 +0300

    fix

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import time
 import logging
 import asyncio
-from datetime import datetime, timedelta, time as dtime
+from datetime import datetime, timedelta
 from pytz import timezone, utc
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
@@ -10,16 +10,15 @@ from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_applicati
 from aiohttp import web
 from config import BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from services.facts import send_facts
-# from services.movies_scheduler_service import send_movies
 from services.quiz_sender import send_quiz
 from services.phrases_sender import send_phrases
-from services.db import mongo  # Імпорт MongoDB
+from services.db import mongo  # Import MongoDB
 from dotenv import load_dotenv
 from handlers.start import register_start_handlers
 from handlers.test import register_test_handlers
 from handlers.interview import register_interview_handlers
 
-# Загрузка конфигурации из .env файла
+# Load configuration from .env file
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -58,9 +57,6 @@ if not logger.hasHandlers():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-# Adding a debug statement to verify logger configuration
-# logger.info("Журнал успешно настроен")
-
 # Initialize the bot with the token from the config file
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot)
@@ -81,10 +77,10 @@ async def schedule_task(func, hour, minute, interval='daily', day_of_week=None, 
             if now > next_run:
                 next_run += timedelta(weeks=1)
         else:
-            raise ValueError("Неверный интервал или день_недели не указан для недельного интервала")
+            raise ValueError("Invalid interval or day_of_week not specified for weekly interval")
 
         sleep_time = (next_run - now).total_seconds()
-        logger.info(f"Запланировано {func.__name__} запуск на {next_run}")
+        logger.info(f"Scheduled {func.__name__} to run at {next_run}")
         await asyncio.sleep(sleep_time)
         await func(*args)
         await asyncio.sleep(1)  # Small sleep to prevent potential tight loop issues
@@ -101,16 +97,15 @@ async def on_startup(app):
     # Test MongoDB connection
     try:
         await mongo.client.server_info()
-        logger.info("Успешное подключение к MongoDB")
+        logger.info("Successfully connected to MongoDB")
     except Exception as e:
-        logger.error(f"Не удалось подключиться к MongoDB: {e}")
+        logger.error(f"Failed to connect to MongoDB: {e}")
 
     # Schedule tasks
-    asyncio.create_task(schedule_task(send_quiz, 9, 0, 'daily', bot, CHANNEL_ID))
-    asyncio.create_task(schedule_task(send_quiz, 17, 0, 'daily', bot, CHANNEL_ID))
-    asyncio.create_task(schedule_task(send_phrases, 12, 0, 'daily', bot, CHANNEL_ID))
-    asyncio.create_task(schedule_task(send_facts, 15, 0, 'weekly', 2, bot, CHANNEL_ID))  # Wednesday
-    # asyncio.create_task(schedule_task(send_movies, 00, 49, 'weekly', 3, bot))  # Saturday
+    asyncio.create_task(schedule_task(send_quiz, 11, 20, 'daily', None, bot, CHANNEL_ID))
+    asyncio.create_task(schedule_task(send_quiz, 17,46, 'daily', None, bot, CHANNEL_ID))
+    asyncio.create_task(schedule_task(send_phrases, 13, 47, 'daily', None, bot))
+    asyncio.create_task(schedule_task(send_facts, 15, 48, 'weekly', 2, bot, CHANNEL_ID))  # Wednesday
 
 @dp.message(Command("status"))
 async def status_command(message: types.Message):
@@ -127,7 +122,7 @@ async def handle_index(request):
     return web.Response(text="Hello! Everything is working!", content_type='text/html')
 
 async def main():
-    logger.info(f"Текущее системное время: {datetime.now(KYIV_TZ)}")
+    logger.info(f"Current system time: {datetime.now(KYIV_TZ)}")
 
     app = web.Application()
     app.on_startup.append(on_startup)
