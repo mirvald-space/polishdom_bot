17e435a4e016d393af2729440c5a9e6bb776bc78
meghost77 <vadim.mirvald@freelancehunt.com>
2024-06-28 23:39:29 +0300

    add keep_alive

--- bot.py
+++ bot.py
@@ -59,8 +59,8 @@ async def on_startup(app):
         if 'func' in task and 'hour' in task and 'minute' in task and 'interval' in task:
             task_name = task['func']
             task_time = f"{task['hour']:02}:{task['minute']:02}"
-            logger.info(f"Scheduled {task_name} to run at {
-                        task_time} with interval {task['interval']}")
+            logger.info(f"Scheduled {task_name} to run at
+                        {task_time} with interval {task['interval']}")
             print(f"Scheduled {task_name} to run at {
                   task_time} with interval {task['interval']}")
         else:
