75eb8555e68c0b2b4d7d0e0db09870da646bfa57
meghost77 <vadim.mirvald@freelancehunt.com>
2024-06-28 23:44:56 +0300

    add keep_alive

--- bot.py
+++ bot.py
@@ -56,11 +56,11 @@ async def on_startup(app):
     logger.info("Successfully connected to MongoDB")
     print(f"SCHEDULE_TASKS: {SCHEDULE_TASKS}")
     for task in SCHEDULE_TASKS:
-        if 'func' in task and 'hour' in task and 'minute' in task and 'interval' in task:
+        if all(key in task for key in ['func', 'hour', 'minute', 'interval']):
             task_name = task['func']
             task_time = f"{task['hour']:02}:{task['minute']:02}"
-            logger.info(f"Scheduled {task_name} to run at "
-                        f"{task_time} with interval {task['interval']}")
+            logger.info(f"Scheduled {task_name} to run at {
+                        task_time} with interval {task['interval']}")
             print(f"Scheduled {task_name} to run at {
                   task_time} with interval {task['interval']}")
         else:
