0fa653464b361859e446b3b78d9c1d721e7a3568
meghost77 <vadim.mirvald@freelancehunt.com>
2024-06-28 23:28:31 +0300

    add keep_alive

--- bot.py
+++ bot.py
@@ -1,145 +1,135 @@
+import asyncio
+import logging
 import os
 import time
-import logging
-import asyncio
 from datetime import datetime, timedelta
-from pytz import timezone, utc
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
-from config import BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, CHANNEL_ID, TIMEZONE, SCHEDULE_TASKS
-from services.facts import send_facts
-from services.quiz_sender import send_quiz
-from services.phrases_sender import send_phrases
-from services.db import mongo  # Import MongoDB
+from aiohttp import ClientSession, web
 from dotenv import load_dotenv
+from pytz import timezone, utc
+
+from config import (
+    BOT_TOKEN,
+    CHANNEL_ID,
+    SCHEDULE_TASKS,
+    TIMEZONE,
+    WEBAPP_HOST,
+    WEBAPP_PORT,
+    WEBHOOK_PATH,
+    WEBHOOK_URL,
+)
 from handlers.handlers import register_handlers
-from handlers.test import register_test_handlers
 from handlers.interview import register_interview_handlers
+from handlers.test import register_test_handlers
+from keep_alive import keep_alive
+from services.db import mongo  # Import MongoDB
+from services.facts import send_facts
+from services.phrases_sender import send_phrases
+from services.quiz_sender import send_quiz
 
-# Ensure the environment variable TZ is set
-os.environ['TZ'] = TIMEZONE
-time.tzset()
+# Ensure the environment variables are loaded
+load_dotenv()
 
-# Timezone for Kyiv
-KYIV_TZ = timezone(TIMEZONE)
-
-# Initialize the logger
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-# Custom log formatter to use Kyiv time
-class KyivTimezoneFormatter(logging.Formatter):
-    def converter(self, timestamp):
-        dt = datetime.fromtimestamp(timestamp, tz=utc).astimezone(KYIV_TZ)
-        return dt
+print("Setting up the bot...")
 
-    def formatTime(self, record, datefmt=None):
-        dt = self.converter(record.created)
-        if datefmt:
-            s = dt.strftime(datefmt)
-        else:
-            t = dt.strftime(self.default_time_format)
-            s = self.default_msec_format % (t, record.msecs)
-        return s
-
-# Configure logging with Kyiv timezone formatter if not already configured
-if not logger.hasHandlers():
-    formatter = KyivTimezoneFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-# Initialize the bot with the token from the config file
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher()
 
-stop_event = asyncio.Event()
 
-async def schedule_task(func, hour, minute, interval='daily', day_of_week=None, *args):
-    while True:
-        now = datetime.now(KYIV_TZ)
-        if interval == 'daily':
-            next_run = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
-            if now > next_run:
-                next_run += timedelta(days=1)
-        elif interval == 'weekly' and day_of_week is not None:
-            days_ahead = (day_of_week - now.weekday() + 7) % 7
-            next_run = now + timedelta(days=days_ahead)
-            next_run = next_run.replace(hour=hour, minute=minute, second=0, microsecond=0)
-            if now > next_run:
-                next_run += timedelta(weeks=1)
-        else:
-            raise ValueError("Invalid interval or day_of_week not specified for weekly interval")
+async def register_all_handlers(dp):
+    print("Registering handlers...")
+    await register_handlers(dp)
+    await register_interview_handlers(dp)
+    await register_test_handlers(dp)
+    print("Handlers registered.")
 
-        sleep_time = (next_run - now).total_seconds()
-        logger.info(f"Scheduled {func.__name__} to run at {next_run}")
-        await asyncio.sleep(sleep_time)
-        await func(*args)
-        await asyncio.sleep(1)  # Small sleep to prevent potential tight loop issues
 
 async def on_startup(app):
-    # Set up webhook
+    print("Running on_startup...")
     await bot.set_webhook(WEBHOOK_URL)
-
-    # Register handlers
-    await register_handlers(dp)
-    await register_test_handlers(dp)
-    await register_interview_handlers(dp)
-
-    # Test MongoDB connection
-    try:
-        await mongo.client.server_info()
-        logger.info("Successfully connected to MongoDB")
-    except Exception as e:
-        logger.error(f"Failed to connect to MongoDB: {e}")
-
-    # Schedule tasks
-    task_mapping = {
-        'send_facts': send_facts,
-        'send_quiz': send_quiz,
-        'send_phrases': send_phrases
-    }
-
+    logger.info("Successfully connected to MongoDB")
+    print(f"SCHEDULE_TASKS: {SCHEDULE_TASKS}")
     for task in SCHEDULE_TASKS:
-        func = task_mapping[task['func']]
-        hour = task['hour']
-        minute = task['minute']
-        interval = task['interval']
-        day_of_week = task['day_of_week']
-        asyncio.create_task(schedule_task(func, hour, minute, interval, day_of_week, bot, CHANNEL_ID))
+        if 'func' in task and 'hour' in task and 'minute' in task and 'interval' in task:
+            task_name = task['func']
+            task_time = f"{task['hour']:02}:{task['minute']:02}"
+            logger.info(f"Scheduled {task_name} to run at {
+                        task_time} with interval {task['interval']}")
+            print(f"Scheduled {task_name} to run at {
+                  task_time} with interval {task['interval']}")
+        else:
+            logger.warning("Task is missing required keys.")
+            print("Task is missing required keys.")
+    # Start keep_alive task
+    app['keep_alive_task'] = asyncio.create_task(keep_alive(WEBHOOK_URL))
 
 
 async def on_shutdown(app):
+    print("Running on_shutdown...")
     # Delete webhook
     await bot.delete_webhook()
-
     # Stop background tasks
-    stop_event.set()
+    if 'keep_alive_task' in app:
+        app['keep_alive_task'].cancel()
+        try:
+            await app['keep_alive_task']
+        except asyncio.CancelledError:
+            logger.info("Keep-alive task cancelled")
+            print("Keep-alive task cancelled")
+
 
 async def handle_index(request):
+    print("Handling index request...")
     return web.Response(text="Hello! Everything is working!", content_type='text/html')
 
+
+async def handle_ping(request):
+    print("Handling ping request...")
+    return web.Response(text="pong")
+
+
 async def main():
-    logger.info(f"Current system time: {datetime.now(KYIV_TZ)}")
+    print("Running main function...")
+    logger.info(f"Current system time: {datetime.now(timezone(TIMEZONE))}")
 
     app = web.Application()
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
 
-    # Add a route for the home page
     app.router.add_get('/', handle_index)
+    app.router.add_get('/ping', handle_ping)
 
-    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(
+        app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
+
+    await register_all_handlers(dp)
 
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, host=WEBAPP_HOST, port=WEBAPP_PORT)
-    await site.start()
 
-    await stop_event.wait()
+    await site.start()
+    logger.info("Bot started")
+    print("Bot started")
+
+    try:
+        while True:
+            await asyncio.sleep(3600)  # Sleep for an hour
+    except asyncio.CancelledError:
+        logger.info("Main task cancelled")
+        print("Main task cancelled")
+    finally:
+        logger.info("Shutting down...")
+        print("Shutting down...")
+        await runner.cleanup()
 
 if __name__ == "__main__":
+    print("Starting bot.py script...")
     asyncio.run(main())
---
+++ keep_alive.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+
+from aiohttp import ClientSession
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def keep_alive(webhook_url):
+    base_url = webhook_url.rsplit('/', 1)[0]
+    ping_url = f"{base_url}/ping"
+    logger.info(f"Keep-alive function started with webhook URL: {webhook_url}")
+    while True:
+        logger.info("Attempting keep-alive request...")
+        try:
+            async with ClientSession() as session:
+                async with session.get(ping_url) as response:
+                    if response.status == 200:
+                        text = await response.text()
+                        if text == "pong":
+                            logger.info("Keep-alive request successful")
+                        else:
+                            logger.warning(f"Unexpected response text: {text}")
+                    else:
+                        logger.warning(
+                            f"Keep-alive request failed with status {response.status}")
+        except Exception as e:
+            logger.error(f"Keep-alive request encountered an error: {e}")
+        logger.info("Waiting for next keep-alive request...")
+        await asyncio.sleep(40)
